1855e25c3c1eb9b7aebdad31b803f3f25d2b346e
petitviolet <violethero0820@gmail.com>
2020-07-22 23:07:28 +0900

    Implemented Myvec as mutable vector

--- sandbox/src/myvec.rs
+++ sandbox/src/myvec.rs
@@ -1,30 +1,55 @@
 #[derive(Debug)]
 pub struct Myvec<T> {
   items: Box<[T]>,
+  pub len: usize,
 }
 
-impl<T: Clone> Myvec<T> {
-  pub fn new() -> Self {
+impl<T: Default> Myvec<T> {
+  pub fn new(capacity: usize) -> Self {
     Self {
-      items: vec![].into_boxed_slice(),
+      items: Self::allocate_in_heap(capacity),
+      len: 0,
     }
   }
-  fn from(items: &[T]) -> Self {
-    Self {
-      items: items.to_vec().into_boxed_slice(),
-    }
+  pub fn empty() -> Self {
+    Self::new(0)
   }
-  pub fn size(&self) -> usize {
+
+  fn allocate_in_heap(capacity: usize) -> Box<[T]> {
+    std::iter::repeat_with(Default::default)
+      .take(capacity)
+      .collect::<Vec<T>>()
+      .into_boxed_slice()
+  }
+
+  pub fn capacity(&self) -> usize { 
     self.items.len()
   }
 
-  pub fn append(&self, item: T) -> Self {
-    let items = [self.items.clone(), Box::new([item])].concat();
-    Self::from(&items)
+  pub fn append(&mut self, item: T) -> () {
+    if self.len >= self.capacity() {
+      self.ensure_capacity();
+    }
+    self.items[self.len] = item;
+    self.len += 1;
+  }
+
+  fn ensure_capacity(&mut self) -> () { 
+    if self.capacity() == 0 {
+      self.items = Self::allocate_in_heap(1);
+    } else {
+      let new_items = Self::allocate_in_heap(self.capacity() * 2);
+      // I think this is not thread-safe
+      let old_items = std::mem::replace(&mut self.items, new_items);
+      // into_vec/into_vec convert type without copying data
+      for (i, item) in old_items.into_vec().into_iter().enumerate() {
+        self.items[i] = item;
+      }
+    }
   }
 
   pub fn fetch(&self, index: usize) -> Option<&T> {
-    if index < self.size() {
+    if index < self.len {
       Some(&self.items[index])
     } else {
       None
@@ -36,20 +61,38 @@ impl<T: Clone> Myvec<T> {
       None => default,
     }
   }
+  pub fn pop(&mut self) -> Option<T> { 
+    if self.len == 0 { None }
+    else {
+      let last_item = std::mem::replace(&mut self.items[self.len - 1], Default::default());
+      self.len -= 1;
+      Some(last_item)
+    }
+  }
 }
 
 pub fn play_myvec() -> () {
-  let myvec: Myvec<i32> = Myvec::new();
+  let mut myvec: Myvec<i32> = Myvec::empty();
   println!("myvec: {:?}", myvec);
   let myvec2 = myvec.append(10);
   println!("myvec: {:?}", myvec);
   println!("myvec2: {:?}", myvec2);
-  let myvecs = (0..5).map(|i| myvec.append(i)).collect::<Vec<_>>();
-  println!("myvecs: {:?}", myvecs);
+  (0..5).for_each(|i| myvec.append(i));
+  println!("myvecs: {:?}", myvec);
 
-  let appended = (0..5).fold(Myvec::new(), |acc, i| acc.append(format!("item-{}", i)));
+  let mut appended = (0..5).fold(Myvec::empty(), |mut acc, i| {
+    acc.append(format!("item-{}", i));
+    acc
+  });
   println!("appended: {:?}", appended);
-  println!("appended.fetch(2): {:?}", appended.fetch(2));
+  let item_2 = appended.fetch(2);
+  println!("appended.fetch(2): {:?}", item_2);
+  // appended.append("item-new".to_string()); // mutable borrow occurs here
+  println!("appended.fetch(2): {:?}", item_2);
+
+  println!("before pop: {:?}", appended);
+  println!("pop: {:?}", appended.pop());
+  println!("after pop: {:?}", appended);
   println!(
     "appended.fetch_or(10, \"foo\"): {:?}",
     appended.fetch_or(10, &"foo".to_string())
