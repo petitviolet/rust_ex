1b906b02f8cbbde14cc6c9aef33f71c3c7846e30
petitviolet <violethero0820@gmail.com>
2020-07-28 23:38:30 +0900

    Applied `cargo fmt`

--- sandbox/src/ex_trait.rs
+++ sandbox/src/ex_trait.rs
@@ -1,86 +1,99 @@
 #[derive(Debug)]
 struct CartesianCoord {
-  x: f64,
-  y: f64,
+    x: f64,
+    y: f64,
 }
 
 #[derive(Debug)]
 struct PolarCoord {
-  r: f64,
-  theta: f64,
+    r: f64,
+    theta: f64,
 }
 
 trait Coordinates {
-  fn to_cartesian(&self) -> CartesianCoord;
-  fn from_catesian<'a>(cart: &CartesianCoord) -> Self;
+    fn to_cartesian(&self) -> CartesianCoord;
+    fn from_catesian<'a>(cart: &CartesianCoord) -> Self;
 }
 
 impl Coordinates for CartesianCoord {
     fn to_cartesian(&self) -> CartesianCoord {
-      CartesianCoord {
-        x: self.x,
-        y: self.y,
-      }
+        CartesianCoord {
+            x: self.x,
+            y: self.y,
+        }
     }
     fn from_catesian(cart: &CartesianCoord) -> Self {
-      CartesianCoord {
-        x: cart.x,
-        y: cart.y,
-      }
+        CartesianCoord {
+            x: cart.x,
+            y: cart.y,
+        }
     }
 }
 
 impl Coordinates for PolarCoord {
     fn to_cartesian(&self) -> CartesianCoord {
-      CartesianCoord {
-        x: self.r * self.theta.cos(),
-        y: self.r * self.theta.sin(),
-      }
+        CartesianCoord {
+            x: self.r * self.theta.cos(),
+            y: self.r * self.theta.sin(),
+        }
     }
 
     fn from_catesian(cart: &CartesianCoord) -> Self {
-      PolarCoord {
-        r: (cart.x * cart.x + cart.y * cart.y).sqrt(),
-        theta: (cart.y / cart.x).atan(),
-      }
+        PolarCoord {
+            r: (cart.x * cart.x + cart.y * cart.y).sqrt(),
+            theta: (cart.y / cart.x).atan(),
+        }
     }
 }
 impl Coordinates for (f64, f64) {
     fn to_cartesian(&self) -> CartesianCoord {
-      CartesianCoord {
-        x: self.0,
-        y: self.1,
-      }
+        CartesianCoord {
+            x: self.0,
+            y: self.1,
+        }
     }
     fn from_catesian(cart: &CartesianCoord) -> Self {
-      (cart.x, cart.y)
+        (cart.x, cart.y)
     }
 }
 
-fn print_point<C: Coordinates + std::fmt::Debug>(c: &C) -> () { 
-  println!("point: {:?}, cartesian: {:?}, polar: {:?}", c, c.to_cartesian(), PolarCoord::from_catesian(&c.to_cartesian()));
+fn print_point<C: Coordinates + std::fmt::Debug>(c: &C) -> () {
+    println!(
+        "point: {:?}, cartesian: {:?}, polar: {:?}",
+        c,
+        c.to_cartesian(),
+        PolarCoord::from_catesian(&c.to_cartesian())
+    );
 }
 
 pub fn f() -> () {
-  let point = (2.0, 2.0);
-  print_point(&point);
+    let point = (2.0, 2.0);
+    print_point(&point);
 }
 
 trait Animal {
-  fn berk(&self) -> ();
+    fn berk(&self) -> ();
 }
-struct Dog { name: &'static str, }
+struct Dog {
+    name: &'static str,
+}
 impl Animal for Dog {
-  fn berk(&self) -> () { println!("Dog({})!!!", self.name); }
+    fn berk(&self) -> () {
+        println!("Dog({})!!!", self.name);
+    }
 }
-struct Cat { name: &'static str, }
+struct Cat {
+    name: &'static str,
+}
 impl Animal for Cat {
-  fn berk(&self) -> () { println!("Cat({})!!!", self.name); }
+    fn berk(&self) -> () {
+        println!("Cat({})!!!", self.name);
+    }
 }
 fn berk(animal: &dyn Animal) -> () {
-  animal.berk();
+    animal.berk();
 }
 pub fn g() -> () {
-  berk(&Dog { name: "pochi" });
-  berk(&Cat { name: "tama" });
+    berk(&Dog { name: "pochi" });
+    berk(&Cat { name: "tama" });
 }
--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,5 +1,5 @@
-mod myvec;
 mod ex_trait;
+mod myvec;
 
 fn main() {
     println!("{}", bang("Hello, world"));
@@ -489,48 +489,55 @@ mod refcell {
 }
 
 mod arc {
-  use std::collections::HashSet;
-  use std::error::Error;
-  use std::sync::{Arc, RwLock};
+    use std::collections::HashSet;
+    use std::error::Error;
+    use std::sync::{Arc, RwLock};
 
-  pub fn f() -> Result<(), Box<dyn Error>> {
-    type T = Arc<RwLock<HashSet<String>>>;
-    let names: T = Arc::new(RwLock::new(
-      ["alice".to_string(), "bob".to_string()].iter().cloned().collect::<HashSet<String>>()
-    ));
-    let start = std::time::Instant::now();
-    let elapsed = move || { return std::time::Instant::now().duration_since(start); };
+    pub fn f() -> Result<(), Box<dyn Error>> {
+        type T = Arc<RwLock<HashSet<String>>>;
+        let names: T = Arc::new(RwLock::new(
+            ["alice".to_string(), "bob".to_string()]
+                .iter()
+                .cloned()
+                .collect::<HashSet<String>>(),
+        ));
+        let start = std::time::Instant::now();
+        let elapsed = move || {
+            return std::time::Instant::now().duration_since(start);
+        };
 
-    let read = || -> () {
-      let cloned1 = Arc::clone(&names);
-      std::thread::spawn(move || {
-        let read_names = cloned1.read().map_err(|err| err.to_string());
-        println!("[{:?}]read_names: {:?}", elapsed(), read_names);
-        std::thread::sleep_ms(30);
-        println!("[{:?}]finish sleep", elapsed());
-      });//.join().expect("fail!");
-    };
+        let read = || -> () {
+            let cloned1 = Arc::clone(&names);
+            std::thread::spawn(move || {
+                let read_names = cloned1.read().map_err(|err| err.to_string());
+                println!("[{:?}]read_names: {:?}", elapsed(), read_names);
+                std::thread::sleep_ms(30);
+                println!("[{:?}]finish sleep", elapsed());
+            }); //.join().expect("fail!");
+        };
 
-    let write = || -> () {
-      let cloned2 = Arc::clone(&names);
-      std::thread::spawn(move || {
-        println!("[{:?}]write try to get lock.", elapsed());
-        let write_names = cloned2.write().map_err(|err| err.to_string());
-        println!("[{:?}]write got lock.", elapsed());
-        write_names.map(|mut wn| {
-          println!("[{:?}]write_names: {:?}", elapsed(), wn);
-          wn.insert(format!("now-{:?}", elapsed()));
-        }).expect("!!!");
-      });//.join().expect("fail!");
-    };
+        let write = || -> () {
+            let cloned2 = Arc::clone(&names);
+            std::thread::spawn(move || {
+                println!("[{:?}]write try to get lock.", elapsed());
+                let write_names = cloned2.write().map_err(|err| err.to_string());
+                println!("[{:?}]write got lock.", elapsed());
+                write_names
+                    .map(|mut wn| {
+                        println!("[{:?}]write_names: {:?}", elapsed(), wn);
+                        wn.insert(format!("now-{:?}", elapsed()));
+                    })
+                    .expect("!!!");
+            }); //.join().expect("fail!");
+        };
 
-    read();
-    write();
-    read();
-    write();
-    read();
+        read();
+        write();
+        read();
+        write();
+        read();
 
-    std::thread::sleep_ms(100);
-    Ok(())
-  }
+        std::thread::sleep_ms(100);
+        Ok(())
+    }
 }
